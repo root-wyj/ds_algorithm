7f2d92ab4bff149c86cf79a87b3ed695650b990d
kim_jie <997269684@qq.com>
2018-06-02 10:08:58 +0800

    x

--- src/com/wyj/ds_algorithm/tree/imp/BSTree.java
+++ src/com/wyj/ds_algorithm/tree/imp/BSTree.java
@@ -1,5 +1,10 @@
 package com.wyj.ds_algorithm.tree.imp;
 
+import com.wyj.ds_algorithm.util.TreeUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二叉搜索树、二叉排序树(Binary Sort|Search Tree)
  * 注意： 该版本并不支持 相等的情况，相等的情况建议使用链表 串在某一个节点上
@@ -10,10 +15,10 @@ package com.wyj.ds_algorithm.tree.imp;
 public class BSTree<T extends Comparable<T>> {
 
 	public static class BSTNode<V extends Comparable<V>> {
-		BSTNode<V> parent;
-		BSTNode<V> left;
-		BSTNode<V> right;
-		V data;
+		public BSTNode<V> parent;
+		public BSTNode<V> left;
+		public BSTNode<V> right;
+		public V data;
 		
 		BSTNode(V data, BSTNode<V> parent, BSTNode<V> left, BSTNode<V> right) {
 			this.data = data;
@@ -27,14 +32,40 @@ public class BSTree<T extends Comparable<T>> {
 		void hook(BSTNode<H> node);
 	}
 	
-	private BSTNode<T> mRoot;
+	public BSTNode<T> mRoot;
 	
-	public BSTree(T root) {
-		mRoot = new BSTNode<>(root, null, null, null);
+	public BSTree() {
+		mRoot = null;
+	}
+
+	public BSTree(T data) {
+		mRoot = new BSTNode<>(data, null, null, null);
+	}
+
+	private BSTNode<T> maximum(BSTNode<T> node) {
+		if (node == null) return null;
+
+		while(node.right != null)
+			node = node.right;
+
+		return node;
+	}
+
+	private BSTNode<T> minimum(BSTNode<T> node) {
+		if (node == null) return null;
+
+		while(node.left != null)
+			node = node.left;
+
+		return node;
 	}
 	
 	// 比到没朋友 方法
 	public void insert(T data) {
+		if (mRoot == null) {
+			mRoot = new BSTNode<>(data, null, null, null);
+			return;
+		}
 		
 		BSTNode<T> parent = mRoot;
 		BSTNode<T> node = mRoot;
@@ -66,9 +97,86 @@ public class BSTree<T extends Comparable<T>> {
 		
 //		insert(mRoot, data);
 	}
-	
+
+	/**
+	 * 该节点的前驱节点(感觉我理解的不太对。。。)
+	 * <p>前驱：有序打印 该节点的前一个节点 就是它的前驱节点</p>
+	 * @param node
+	 * @return
+	 */
+	private BSTNode<T> predecessor(BSTNode<T> node) {
+		// 如果存在左子树，左子树的最大的节点就是该节点的前驱节点
+		if (node.left != null)
+			return maximum(node.left);
+
+		// 不存在 左子树
+		// 如果该节点是右孩子 父节点就是前驱节点
+		// 如果该节点是左孩子 该节点的最低父节点p1，并且该父节点p1是其父节点p2的右节点，那么p2就是前驱
+		if (node.parent.right == node)
+			return node.parent;
+
+		BSTNode<T> parent = node.parent;
+		if (parent != null && parent.left==node) {
+			node = parent;
+			parent = parent.parent;
+		}
+		return parent;
+	}
+
+	/**
+	 * 该节点的后继节点
+	 * <p>后继：有序打印 该节点的后一个节点 就是它的后继节点</p>
+	 * @param node
+	 * @return
+	 */
+	private BSTNode<T> successor(BSTNode<T> node) {
+		if (node.right != null)
+			return minimum(node.right);
+
+		// 如果x没有右孩子。则x有以下两种可能：
+		// (01) x是"一个左孩子"，则"x的后继结点"为 "它的父结点"。
+		// (02) x是"一个右孩子"，则查找"x的最低的父结点，并且该父结点要具有左孩子"，找到的这个"最低的父结点"就是"x的后继结点"。
+		BSTNode<T> parent = node.parent;
+		while ((parent!=null) && (node==parent.right)) {
+			node = parent;
+			parent = parent.parent;
+		}
+
+		return parent;
+	}
+
 	public void delete(T data) {
-		
+		BSTNode<T> node = search(data);
+		if (node == null) return;
+
+		// child都不为空 直接删除
+		if (node.left != null && node.right != null) {
+			// 获取后继节点 并交换内容
+			BSTNode<T> successor = successor(node);
+			node.data = successor.data;
+
+			node = successor;
+		}
+
+		BSTNode<T> child = null;
+
+		// 交换完位置后 只剩下两种情况，只有一个节点 或者没有节点
+		if (node.left != null) {
+			child = node.left;
+		} else {
+			child = node.right;
+		}
+
+		if (child != null) {
+			child.parent = node.parent;
+		}
+
+		if (node.parent.left == node) {
+			node.parent.left = child;
+		} else {
+			node.parent.right = child;
+		}
+
 	}
 	
 	public BSTNode<T> search(T data) {
@@ -111,23 +219,52 @@ public class BSTree<T extends Comparable<T>> {
 			traverseLeft(node.right, hook);
 		}
 	}
+
+	public void traverseMid(BSTNode<T> node, TraverseHook<T> hook) {
+		hook.hook(node);
+
+		if (node.left !=null) {
+			traverseMid(node.left, hook);
+		}
+
+
+		if (node.right != null) {
+			traverseMid(node.right, hook);
+		}
+	}
 	
 	public static void main(String[] args) {
-		BSTree<Integer> tree = new BSTree<Integer>(30);
-		tree.insert(10);
-		tree.insert(20);
-		tree.insert(5);
-		tree.insert(15);
-		tree.insert(25);
-		
-		tree.insert(50);
-		tree.insert(40);
+
+		int[] array = {50, 30, 80, 20, 35, 34, 32, 40, 70, 75, 100};
+
+		BSTree<Integer> tree = new BSTree<Integer>();
+
+		for (int i : array) {
+			tree.insert(i);
+		}
+
+		final List<Integer> list = new ArrayList<>();
 		tree.traverseLeft(tree.mRoot, new TraverseHook<Integer>() {
 			@Override
 			public void hook(BSTNode<Integer> node) {
-				System.out.println(node.data);
+				list.add(node.data);
 			}
 		});
+
+		System.out.println(list);
+
+		tree.delete(40);
+
+		list.clear();
+		tree.traverseLeft(tree.mRoot, new TraverseHook<Integer>() {
+			@Override
+			public void hook(BSTNode<Integer> node) {
+				list.add(node.data);
+			}
+		});
+		System.out.println(list);
+
+		TreeUtils.print(tree);
 	}
 	
 	
--- src/com/wyj/ds_algorithm/util/TreeUtils.java
+++ src/com/wyj/ds_algorithm/util/TreeUtils.java
@@ -1,5 +1,7 @@
 package com.wyj.ds_algorithm.util;
 
+import com.wyj.ds_algorithm.tree.imp.BSTree;
+
 /**
  * Created
  * Author: wyj
@@ -7,4 +9,51 @@ package com.wyj.ds_algorithm.util;
  * Date: 2018/5/30
  */
 public class TreeUtils {
+    //------------下面代码是用于输出树的工具代码------------------------
+
+    public static final String  PREFIX_BRANCH = "├";//树枝
+    public static final String  PREFIX_TRUNK  = "│ ";//树干
+    public static final String  PREFIX_LEAF   = "└";//叶子
+    public static final String  PREFIX_EMP    = "  ";//空
+    public static final String  PREFIX_LEFT   = "─L─";//左
+    public static final String  PREFIX_RIGTH  = "─R─";//右
+
+    private static boolean hasChild(BSTree.BSTNode node){
+        return node.left != null || node.right != null;
+    }
+
+    public static void print(BSTree tree){
+        if(tree != null && tree.mRoot != null){
+            System.out.println(tree.mRoot.data);
+            print(tree.mRoot, "");
+        }
+    }
+
+    public static void print(BSTree.BSTNode node, String prefix){
+        if(prefix == null){
+            prefix = "";
+        } else {
+            prefix = prefix.replace(PREFIX_BRANCH, PREFIX_TRUNK);
+            prefix = prefix.replace(PREFIX_LEAF, PREFIX_EMP);
+        }
+        if(hasChild(node)){
+            if(node.right != null){
+                System.out.println(prefix + PREFIX_BRANCH + PREFIX_RIGTH + node.right.data);
+                if(hasChild(node.right)){
+                    print(node.right, prefix + PREFIX_BRANCH);
+                }
+            } else {
+                System.out.println(prefix + PREFIX_BRANCH + PREFIX_RIGTH);
+            }
+
+            if(node.left != null){
+                System.out.println(prefix + PREFIX_LEAF + PREFIX_LEFT + node.left.data);
+                if(hasChild(node.left)){
+                    print(node.left, prefix + PREFIX_LEAF);
+                }
+            } else {
+                System.out.println(prefix + PREFIX_LEAF + PREFIX_LEFT);
+            }
+        }
+    }
 }
